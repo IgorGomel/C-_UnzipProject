9c73312037e882765586215c55ed245ffa604cd6
IgorGomel <Gomel051184@gmail.com>
2019-10-14 15:47:32 +0300

    first commit

---
+++ Unzipping/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Unzipping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btUnzipCSV = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripOpenFile = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btUnzipCSV
+            // 
+            this.btUnzipCSV.Enabled = false;
+            this.btUnzipCSV.Location = new System.Drawing.Point(12, 69);
+            this.btUnzipCSV.Name = "btUnzipCSV";
+            this.btUnzipCSV.Size = new System.Drawing.Size(94, 47);
+            this.btUnzipCSV.TabIndex = 0;
+            this.btUnzipCSV.Text = "Розархівувати CSV-файли";
+            this.btUnzipCSV.UseVisualStyleBackColor = true;
+            this.btUnzipCSV.Click += new System.EventHandler(this.btUnzipCSV_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripOpenFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(233, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripOpenFile
+            // 
+            this.toolStripOpenFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripOpenFile.Image")));
+            this.toolStripOpenFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripOpenFile.Name = "toolStripOpenFile";
+            this.toolStripOpenFile.Size = new System.Drawing.Size(105, 22);
+            this.toolStripOpenFile.Text = "Вибрати файл";
+            this.toolStripOpenFile.Click += new System.EventHandler(this.toolStripOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 146);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btUnzipCSV);
+            this.Name = "Form1";
+            this.Text = "Робота з архівом";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btUnzipCSV;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripOpenFile;
+    }
+}
+
---
+++ Unzipping/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace Unzipping
+{
+    
+    public partial class Form1 : Form
+    {
+        string tempFileName;
+        ZipArchive archive;
+        public string ourFile;
+        string extractPath = @".\extract";
+
+        public void A()
+        { 
+            Directory.CreateDirectory(extractPath);
+            
+            archive = ZipFile.Open(ourFile, ZipArchiveMode.Update);
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                if (entry.FullName.EndsWith(".csv"))
+                {
+                    entry.ExtractToFile(extractPath + "\\" + entry.Name, true);
+                    tempFileName = extractPath + "\\" + entry.Name;
+                    this.B(tempFileName);
+                }
+            }
+
+            MessageBox.Show("Файли розархівіровано!");
+
+            var folder = Directory.GetFiles(extractPath);
+      
+            foreach (var entry in folder)
+            {
+                File.Delete(entry);
+            }
+
+            Directory.Delete(extractPath);
+            btUnzipCSV.Enabled = false;
+            
+        }
+
+        public void B(string tmp)
+        {
+            MessageBox.Show("Медод Б викликано для " + tmp +" !");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripOpenFile_Click(object sender, EventArgs e)
+        {
+            
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.EndsWith(".zip"))
+            {
+                ourFile = openFileDialog1.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Виберіть архівний файл з розширенням .zip");
+                btUnzipCSV.Enabled = false;
+                return;
+            }
+            btUnzipCSV.Enabled = true;
+                 
+        }
+
+        private void btUnzipCSV_Click(object sender, EventArgs e)
+        {
+            this.A();
+        }
+    }
+}
